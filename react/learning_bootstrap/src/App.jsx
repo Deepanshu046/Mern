192ede18e1963a9594acb203c4ca0540495c15cb
Deepanshu Pandey <deepanshupandey046@gmail.com>
2026-06-07 22:07:02 +0530

    kota pura kr rha hu

--- react/learning_bootstrap/src/App.jsx
+++ react/learning_bootstrap/src/App.jsx
@@ -3,36 +3,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let arr = ["apple" , "mango" , "Litchi"]
 
   return (
-    <div>
-      <div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-<div class="alert alert-secondary" role="alert">
-  A simple secondary alert—check it out!
-</div>
-<div class="alert alert-success" role="alert">
-  A simple success alert—check it out!
-</div>
-<div class="alert alert-danger" role="alert">
-  A simple danger alert—check it out!
-</div>
-<div class="alert alert-warning" role="alert">
-  A simple warning alert—check it out!
-</div>
-<div class="alert alert-info" role="alert">
-  A simple info alert—check it out!
-</div>
-<div class="alert alert-light" role="alert">
-  A simple light alert—check it out!
-</div>
-<div class="alert alert-dark" role="alert">
-  A simple dark alert—check it out!
-</div>
-    </div>
-  )
+    <>
+      <ul>
+        {arr.map((item)=>(
+          <li key={item}>{console.log(item)}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App
